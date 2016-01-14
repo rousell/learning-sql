5c54fc56760667a0a10cda664e118601328b7cc9
Lauren Rouse <lauren.l.rouse@gmail.com>
2016-01-13 21:09:40 -0600

    LR commit of 3.sql

--- 3.sql
+++ 3.sql
@@ -1,1 +1,1 @@
-SELECT I.InvoiceId,C.CustomerId,C.FirstName,C.LastName,Country FROM Customer AS C JOIN Invoice AS I WHERE C.Country == 'Brazil'
+SELECT I.InvoiceId,I.InvoiceDate,C.CustomerId,C.FirstName,C.LastName,Country FROM Customer AS C JOIN Invoice AS I WHERE C.Country == 'Brazil'

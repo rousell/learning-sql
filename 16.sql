4808bbad214a3fef4a7c9ccc45d81991877fdf83
Lauren Rouse <lauren.l.rouse@gmail.com>
2016-01-18 17:21:31 -0600

    LR final commit of 16.sql

---
+++ 16.sql
@@ -0,0 +1,16 @@
+SELECT
+  SQ.Name AS "Album Name",
+  SQ.MediaType AS "Media Type",
+  Genre.Name AS "Genre",
+FROM (SELECT
+        T.Name,
+        T.Composer,
+        T.Milliseconds,
+        T.Bytes,
+        T.UnitPrice,
+        MediaType.Name AS "MediaType",
+        T.GenreId
+      FROM Track AS T JOIN MediaType ON T.MediaTypeId == MediaType.MediaTypeId) AS SQ
+JOIN Genre ON Genre.GenreId == SQ.GenreId
+
+-- Genre Id is still in the resultant Table

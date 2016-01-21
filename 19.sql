25e2131b707b5b213999e1c8138f88a7b0f7728b
Lauren Rouse <lauren.l.rouse@gmail.com>
2016-01-20 18:02:17 -0600

    LR commit of 19.sql

---
+++ 19.sql
@@ -0,0 +1,14 @@
+SELECT
+  Employee.FirstName AS "Sales Agent First Name",
+  Employee.LastName AS "Sales Agent Last Name",
+  MAX(SQ.YearlyTotal) AS "Yearly Total",
+  SQ.InvoiceYear
+FROM(SELECT
+      ROUND(SUM(I.Total),2) AS "YearlyTotal",
+      I.InvoiceDate,
+      C.CustomerId,
+      C.SupportRepId,
+      SUBSTR(I.InvoiceDate,1,4) AS "InvoiceYear"
+    FROM Customer AS C JOIN Invoice as I ON I.CustomerId == C.CustomerId GROUP BY SUBSTR(I.InvoiceDate,1,4), C.SupportRepId) AS SQ
+JOIN Employee ON SQ.SupportRepId == Employee.EmployeeId
+WHERE InvoiceYear =="2009"

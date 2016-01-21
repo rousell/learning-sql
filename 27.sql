62cf94b57f04cf911512dd7f78542bb65f3641cf
Lauren Rouse <lauren.l.rouse@gmail.com>
2016-01-20 19:04:34 -0600

    LR commit of 27.sql

---
+++ 27.sql
@@ -0,0 +1,6 @@
+SELECT MediaType.Name,SUM(SQ.Quantity) AS "Quantity"
+FROM
+  (SELECT * FROM InvoiceLine JOIN Track ON InvoiceLine.TrackId == Track.TrackId) AS SQ
+JOIN MediaType ON MediaType.MediaTypeId == SQ.MediaTypeId
+  GROUP BY MediaType.Name
+  ORDER BY Quantity DESC
